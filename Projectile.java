5a2bd511d7a3790091bb98678a092502a7e77dd6
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-05-20 04:38:52 +0200

    Create Projectile.java

---
+++ Projectile.java
@@ -0,0 +1,18 @@
+// ______   __  __   __  __    
+///\  ___\ /\ \/\ \ /\_\_\_\   
+//\ \ \____\ \ \_\ \\/_/\_\/_  
+// \ \_____\\ \_____\ /\_\/\_\
+//  \/_____/ \/_____/ \/_/\/_/
+
+import javafx.scene.shape.*;
+
+public class Projectile extends Character {
+    public Projectile(int x, int y){
+        super(new Polygon(2, -2, 2, 2, -2, 2, -2, -2), x, y);
+    }
+}
+//keep taking care of your goals, 
+//mental health, self care, recognize 
+//cognitive distortions and rebel against them!!
+//one step at a time, don't be afraid to 
+//ask 4 help or to fail at times.
